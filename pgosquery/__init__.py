99fb2eb5c9bcc1266c6b3ea137c062adcfbe1b4f
shish <shish@shishnet.org>
2014-10-30 13:13:55 +0000

    support many table types and arbitrary columns

--- pgosquery/__init__.py
+++ pgosquery/__init__.py
@@ -1,21 +1,20 @@
 from multicorn import ForeignDataWrapper
 import psutil
-import q
 
 
 class PgOSQuery(ForeignDataWrapper):
 
     def __init__(self, options, columns):
         super(PgOSQuery, self).__init__(options, columns)
+        self.table_type = options.get("tabletype", "processes")
         self.columns = columns
-        q(options)
-        q(columns)
 
     def execute(self, quals, columns):
-        for proc in psutil.process_iter():
-            try:
-                pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
-            except psutil.NoSuchProcess:
-                pass
-            else:
-                yield pinfo
+        if self.table_type == "processes":
+            for proc in psutil.process_iter():
+                try:
+                    pinfo = proc.as_dict(attrs=self.columns.keys())
+                except psutil.NoSuchProcess:
+                    pass
+                else:
+                    yield pinfo
