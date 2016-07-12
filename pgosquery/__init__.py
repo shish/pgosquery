6aaeddb131378a695f3602fbd4f126905f4a3620
Shish <shish@shishnet.org>
2016-07-12 17:30:26 +0100

    tabling

--- pgosquery/__init__.py
+++ pgosquery/__init__.py
@@ -11,48 +11,58 @@ class PgOSQuery(ForeignDataWrapper):
         self.columns = columns
 
     def execute(self, quals, columns):
-        if self.table_type == "processes":
-            for proc in psutil.process_iter():
-                try:
-                    pinfo = proc.as_dict(attrs=self.columns.keys())
-                except psutil.NoSuchProcess:
-                    pass
-                else:
-                    yield pinfo
+        func = getattr(self, self.table_type)
+        for v in func(quals, columns):
+            yield v
 
-        if self.table_type == "listening_ports":
-            for proc in psutil.process_iter():
-                try:
-                    for conn in proc.connections(kind="inet"):
-                        if conn.status == "LISTEN":
-                            yield {
-                                "pid": proc.pid,
-                                "address": conn.laddr[0],
-                                "port": conn.laddr[1],
-                            }
-                except psutil.NoSuchProcess:
-                    pass
-                except psutil.AccessDenied:
-                    pass
+    ###################################################################
+    # Tables
+    ###################################################################
 
-        if self.table_type == "net_connections":
-            types = {
-                socket.SOCK_DGRAM: "udp",
-                socket.SOCK_STREAM: "tcp"
-            }
-            def status(string):
-                return None if string == psutil.CONN_NONE else string
-            for proc in psutil.process_iter():
-                try:
-                    for conn in proc.connections(kind="inet"):
+    def processes(self, quals, columns):
+        for proc in psutil.process_iter():
+            try:
+                pinfo = proc.as_dict(attrs=self.columns.keys())
+            except psutil.NoSuchProcess:
+                pass
+            else:
+                yield pinfo
+
+    def listening_ports(self, quals, columns):
+        for proc in psutil.process_iter():
+            try:
+                for conn in proc.connections(kind="inet"):
+                    if conn.status == "LISTEN":
                         yield {
                             "pid": proc.pid,
-                            "address": conn.laddr[0],
+                            "address": conn.laddr[1],
                             "port": conn.laddr[1],
-                            "type": types[conn.type],
-                            "status": status(conn.status)
+                            # protocol
+                            # family
                         }
-                except psutil.NoSuchProcess:
-                    pass
-                except psutil.AccessDenied:
-                    pass
+            except psutil.NoSuchProcess:
+                pass
+            except psutil.AccessDenied:
+                pass
+
+    def net_connections(self, quals, columns):
+        types = {
+            socket.SOCK_DGRAM: "udp",
+            socket.SOCK_STREAM: "tcp"
+        }
+        def status(string):
+            return None if string == psutil.CONN_NONE else string
+        for proc in psutil.process_iter():
+            try:
+                for conn in proc.connections(kind="inet"):
+                    yield {
+                        "pid": proc.pid,
+                        "address": conn.laddr[0],
+                        "port": conn.laddr[1],
+                        "type": types[conn.type],
+                        "status": status(conn.status)
+                    }
+            except psutil.NoSuchProcess:
+                pass
+            except psutil.AccessDenied:
+                pass
